429c016a38cb940e5ce79825222267cffc32ce00
Ranson <ranson.namba@cloudinary.com>
2022-05-26 14:41:47 -1000

    finishes steps 4-6 tentatively, needs refactor to remove prior URL before appending any second round transformations

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   // Sets Cloudinary config
   $.cloudinary.config({ cloud_name: 'majincloud', secure: false});
@@ -9,23 +8,29 @@ $(document).ready(function() {
   if($.fn.cloudinary_fileupload !== undefined) {
     $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
   }
+  // Delete button onclick();
+  document.getElementById('deleteImageButton').onclick = function(){
+    $.cloudinary.delete_by_token(deleteToken);
+  }
+
 
   // Begin task 1 code
-  uploadButton.onclick = function(){
-     let urlvar = document.getElementById('urlInput').value;
-     let secondParam = {type: "fetch", transformation: [
-          {gravity: "face", height: 300, width: 300, crop: "fill"},
-          {radius: "max"},
-          {fetch_format: "auto"}
-      ]}
-      //start cloudinary fetch code
-      $.cloudinary.image(urlvar, secondParam);
-      let imagePreview = $.cloudinary.imageTag(urlvar, secondParam).toHtml();
-      //end cloudinary fetch code
+  // uploadButton.onclick = function(){
+  //    let urlvar = document.getElementById('urlInput').value;
+  //    let secondParam = {type: "fetch", transformation: [
+  //         {gravity: "face", height: 300, width: 300, crop: "fill"},
+  //         {radius: "max"},
+  //         {fetch_format: "auto"}
+  //     ]}
+  //     //start cloudinary fetch code
+  //     $.cloudinary.image(urlvar, secondParam);
+  //     let imagePreview = $.cloudinary.imageTag(urlvar, secondParam).toHtml();
+  //     //end cloudinary fetch code
 
-      //renders the uploaded image with effects
-      $('.uploadedImage').empty().append(imagePreview);
-  }
+  //     //renders the uploaded image with effects
+  //     $('.uploadedImage').empty().append(imagePreview);
+  // }
+  // End task 1 code
 
   //Prevents redirection when hitting enter in input field
    $("#overLayText").keydown(function(event) {
@@ -35,34 +40,44 @@ $(document).ready(function() {
   });
   //Applies all selected transformations
   transformationButton.onclick = function(){
-     let secondParam = {type: "fetch", transformation: [
-      {gravity: "face", height: 300, width: 300, crop: "fill"},
-      {radius: "max"},
-      {fetch_format: "auto"}
-    ]}
+    //  let secondParam = {type: "fetch", transformation: [
+    //   {gravity: "face", height: 300, width: 300, crop: "fill"},
+    //   {radius: "max"},
+    //   {fetch_format: "auto"}
+    // ]}
     // Check for any transformation checkmarks before submitting
-    $(':checkbox:checked').each(function () {
-    // Need to set this.val() as var to use in the cloudinary.image POST
-        secondParam.transformation.push({effect: $(this).val()});
-    });
-
+    var checkBoxArray = []
+    var checkBoxes = document.querySelectorAll('input[type=checkbox]:checked')
+    for (var i = 0; i < checkBoxes.length; i++) {
+      checkBoxArray.push("e_" + checkBoxes[i].value+"/");
+    }
+    console.log(checkBoxArray);
+  
     let inputText = document.getElementById('overLayText').value;
     if( inputText ){
-      secondParam.transformation.push({overlay: new cloudinary.TextLayer().fontFamily("Arial").fontSize(24).fontWeight("bold").text(inputText)});
+      let finalInputText = "l_text:Arial_200:" + inputText;
+      let transformURL = document.getElementById('widgetURL');
+      let urlToChange = transformURL.getAttribute('src');
+
+      let finalUrlOutput = urlToChange.replace("/upload/", "/upload/" + checkBoxArray.join(",") +finalInputText+"/" + "c_crop/g_custom/");
+
+      transformURL.setAttribute('src', finalUrlOutput);
+
+      // secondParam.transformation.push({overlay: new cloudinary.TextLayer().fontFamily("Arial").fontSize(24).fontWeight("bold").text(inputText)});
     }
     let urlvar = document.getElementById('urlInput').value;
 
     //start cloudinary fetch code
-    $.cloudinary.image(urlvar, secondParam);
-    let imagePreview = $.cloudinary.imageTag(urlvar, secondParam).toHtml();
+    // $.cloudinary.image(urlvar, secondParam);
+    // let imagePreview = $.cloudinary.imageTag(urlvar, secondParam).toHtml();
     //end cloudinary fetch code
 
     //renders the uploaded image with effects
-    console.log(imagePreview);
-    $('.uploadedImage').empty().append(imagePreview);
+    // console.log(imagePreview);
+    // $('.uploadedImage').empty().append(imagePreview);
   }
 });
 
-//https://cdn.pixabay.com/photo/2019/11/08/11/56/cat-4611189__340.jpg
+//
 //https://en.wikipedia.org/wiki/Cat#/media/File:Cat_poster_1.jpg
 //https://images.unsplash.com/photo-1506755855567-92ff770e8d00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3456&q=80
